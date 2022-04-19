a0ed9f02d3e17bed2fa797f0f736c189bbff7ed4
Badalranjit15 <65303330+Badalranjit15@users.noreply.github.com>
2022-04-19 16:29:14 +0530

    Update variables.tf

--- vpc/module/variables.tf
+++ vpc/module/variables.tf
@@ -16,18 +16,6 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of private subnets to create within the VPC"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "public_subnets" {
-  type        = list(string)
-  description = "List of public subnets to create within the VPC"
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
 variable "enable_nat_gateway" {
   type        = bool
   description = "Whether or not to enable a NAT gateway for the private subnets"
@@ -38,4 +26,10 @@ variable "enable_vpn_gateway" {
   type        = bool
   description = "Whether or not to enable a VPN gateway"
   default     = false
+}
+
+variable "subnet_count" {
+  type        = number
+  description = "Whether or not to enable a VPN gateway"
+  default     = 4
 }
