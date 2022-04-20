343abcd6ea0032645c1996b8796860248ac6f93e
Badalranjit15 <65303330+Badalranjit15@users.noreply.github.com>
2022-04-20 11:44:49 +0530

    Update main.tf

--- vpc/module/main.tf
+++ vpc/module/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "private_subnet" {
   cidr_block        = "10.0.0.${count.index * 64}/26"
 #  availability_zone = "${element(data.aws_availability_zones.all.names, count.index)}"
 
-  tags {
+  tags = {
    Name = "${var.vpc_name}-${element(var.availability_zone, count.index)}-${count.index}"
   }
 }
